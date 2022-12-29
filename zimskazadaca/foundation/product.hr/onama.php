8ad04d0141d06d0dd85e35a583a953c9c074b446
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2022-12-29 18:02:27 +0100

    update

--- zimskazadaca/foundation/product.hr/onama.php
+++ zimskazadaca/foundation/product.hr/onama.php
@@ -22,7 +22,7 @@
         <div class="large-12 cell">
           <div class="callout" id="tijelo">
             <div class="onamatext">
-            Product plast jedan je od vodećih skandinavskih proizvođača vakuumski oblikovanih termoplasta. Proizvodimo specijalizirane proizvode izrađene po narudžbi od termoplasta za industriju i druge svrhe. Udio izvoza je značajan iu porastu.<br>
+            Product plast jedan je od vodećih skandinavskih proizvođača vakumski oblikovanih termoplasta. Proizvodimo specijalizirane proizvode izrađene po narudžbi od termoplasta za industriju i druge svrhe. Udio izvoza je značajan i u porastu.<br>
             Tvrtka je osnovana 1972. godine u privatnom je vlasništvu. Položaj 20 km zapadno od Kristiansanda na jugu Norveške omogućuje dobru komunikaciju sa Skandinavijom i ostatkom kontinenta.<br><hr>
             Product Plast opremljen je modernom i učinkovitom proizvodnom opremom te kompetentnim zaposlenicima koji mogu riješiti većinu izazova koje tržište zahtijeva.<br><hr>
             Product Plast ima ambiciozan slogan; ”Pravo rješenje, u pravo vrijeme, s pravom kvalitetom”. <br><hr>
--- zimskazadaca/foundation/product.hr/proces.php
+++ zimskazadaca/foundation/product.hr/proces.php
@@ -9,42 +9,58 @@
       <div class="grid-x grid-padding-x">
         <div class="large-12 cell">
           <div class="callout" id="tijelo">
-            PROCES PROIZVODNJE
-          <ul class="tabs" data-tabs id="example-tabs">
-  <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Tab 1</a></li>
-  <li class="tabs-title"><a href="#panel2">Tab 2</a></li>
-  <li class="tabs-title"><a href="#panel3">Tab 3</a></li>
-  <li class="tabs-title"><a href="#panel4">Tab 4</a></li>
-  <li class="tabs-title"><a href="#panel5">Tab 5</a></li>
-  <li class="tabs-title"><a href="#panel6">Tab 6</a></li>
-</ul>
+            <h3>PROCES PROIZVODNJE</h3>
+              <ul class="tabs" data-tabs id="example-tabs">
+                <li class="tabs-title is-active"><a href="#panel1" aria-selected="true">Dio 1</a></li>
+                <li class="tabs-title"><a href="#panel2">Dio 2</a></li>
+                <li class="tabs-title"><a href="#panel3">Dio 3</a></li>
+                <li class="tabs-title"><a href="#panel4">Dio 4</a></li>
+                <li class="tabs-title"><a href="#panel5">Dio 5</a></li>
+                <li class="tabs-title"><a href="#panel6">Dio 6</a></li>
+              </ul>
 
-<div class="tabs-content" data-tabs-content="example-tabs">
-  <div class="tabs-panel is-active" id="panel1">
-    <p>One</p>
-    <p>Check me out! I'm a super cool Tab panel with text content!</p>
-  </div>
-  <div class="tabs-panel" id="panel2">
-    <p>Two</p>
-    <img class="thumbnail" src="assets/img/generic/rectangle-7.jpg" alt="">
-  </div>
-  <div class="tabs-panel" id="panel3">
-    <p>Three</p>
-    <p>Check me out! I'm a super cool Tab panel with text content!</p>
-  </div>
-  <div class="tabs-panel" id="panel4">
-    <p>Four</p>
-    <img class="thumbnail" src="assets/img/generic/rectangle-2.jpg" alt="">
-  </div>
-  <div class="tabs-panel" id="panel5">
-    <p>Five</p>
-    <p>Check me out! I'm a super cool Tab panel with text content!</p>
-  </div>
-  <div class="tabs-panel" id="panel6">
-    <p>Six</p>
-    <img class="thumbnail" src="assets/img/generic/rectangle-8.jpg" alt="">
-  </div>
-</div>
+              <div class="tabs-content" data-tabs-content="example-tabs">
+                <div class="tabs-panel is-active" id="panel1">    
+                  <div id="proces1">
+                  </div>
+                </div>
+                <div class="tabs-panel" id="panel2">
+                  <div id="proces2">
+                  </div>
+                </div>
+                <div class="tabs-panel" id="panel3">
+                  <div id="proces3">
+                  </div>
+                </div>
+                <div class="tabs-panel" id="panel4">
+                  <div id="proces4">
+                  </div>
+                </div>
+                <div class="tabs-panel" id="panel5">
+                  <div id="proces5">
+                  </div>
+                </div>
+                <div class="tabs-panel" id="panel6">
+                  <div id="proces6">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/crAx1aKPDsg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid-container">
+      <?php require_once 'izbornik.php'; ?>
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="callout" id="tijelo2">          
+            <div id="procesp">
+              Proces uključuje zagrijavanje plastične ploce dok ne omekša, a zatim se prevlaci preko kalupa. Primjenjuje se vakuum koji usisava otopljenu plocu u kalup. Proces hladjenja nastupa nakon zavrsetka s vakumom. List se zatim izbacuje iz kalupa.
+              <br><br>
+              Vakuumsko oblikovanje je drevna tehnika koja se koristi za proizvodnju plastičnih proizvoda. To je najjednostavniji oblik procesa termoformiranja koji za rad koristi vakuumske usisne pumpe.
+            </div>
           </div>
         </div>
         <?php include_once 'podnozje.php'; ?>
