da0cdf53db9080a85c8c7d8b1aee4148e94dd8d7
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2022-12-29 16:16:34 +0100

    update foundation

--- zimskazadaca/foundation/product.hr/index.php
+++ zimskazadaca/foundation/product.hr/index.php
@@ -8,13 +8,36 @@
       <?php require_once 'izbornik.php'; ?>
       <div class="grid-x grid-padding-x">
         <div class="large-12 cell">
-          <div class="callout" id="tijelo">
-          Koristite gornji izbornik
+          <div class="callout" id="tijelo">          
+            <div class="indexslika1">
+            </div>
+            <div class="indextext">
+            <br><br><br><br>
+            Proizvodi vakuumskog oblika koriste se u gotovo svim industrijama. Veličina redovne serije kreće se od 50 do 10 000 jedinica godišnje.
+            Proces karakteriziraju niski troškovi alata, niski početni troškovi, kratka vremena i visok stupanj fleksibilnosti u pogledu odabira materijala.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid-container">
+      <?php require_once 'izbornik.php'; ?>
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="callout" id="tijelo">          
+            <div class="indexslika2">
+            </div>
+            <div class="indextext">
+            <br><br><br>
+            Od naših tipičnih proizvoda možemo spomenuti ploče za automobile: autobuse, kamione, građevinske strojeve i prenamjenu automobila. Industrija rashladnih sredstava: Rashladna oprema za komercijalne kuhinje, rashladne jedinice i druge srodne industrije. Pomoćni proizvodi: Razna pomagala za osobe s invaliditetom, kao što su invalidska kolica, skuteri, dizala. Displej: Razne fronte / paneli za strojeve i strojeve unutar maloprodaje.
+            </div>
           </div>
         </div>
         <?php include_once 'podnozje.php'; ?>
       </div>
     </div>
+
    <?php include_once 'jskripte.php'; ?>
   </body>
 </html>
--- zimskazadaca/foundation/product.hr/onama.php
+++ zimskazadaca/foundation/product.hr/onama.php
@@ -9,12 +9,36 @@
       <div class="grid-x grid-padding-x">
         <div class="large-12 cell">
           <div class="callout" id="tijelo">
-          o nama
+            <div class="onamaslika1">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="grid-container">
+      <?php require_once 'izbornik.php'; ?>
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="callout" id="tijelo">
+            <div class="onamatext">
+            Product plast jedan je od vodećih skandinavskih proizvođača vakuumski oblikovanih termoplasta. Proizvodimo specijalizirane proizvode izrađene po narudžbi od termoplasta za industriju i druge svrhe. Udio izvoza je značajan iu porastu.<br>
+            Tvrtka je osnovana 1972. godine u privatnom je vlasništvu. Položaj 20 km zapadno od Kristiansanda na jugu Norveške omogućuje dobru komunikaciju sa Skandinavijom i ostatkom kontinenta.<br><hr>
+            Product Plast opremljen je modernom i učinkovitom proizvodnom opremom te kompetentnim zaposlenicima koji mogu riješiti većinu izazova koje tržište zahtijeva.<br><hr>
+            Product Plast ima ambiciozan slogan; ”Pravo rješenje, u pravo vrijeme, s pravom kvalitetom”. <br><hr>
+            Nastojimo to postići kroz posebnu posvećenost 4 glavna područja:<br>
+
+            Visoka kompetentnost
+            Fleksibilnost
+            Sigurnost opskrbe
+            Višak / Čvrstoća
+            </div>
           </div>
         </div>
         <?php include_once 'podnozje.php'; ?>
       </div>
     </div>
+
    <?php include_once 'jskripte.php'; ?>
   </body>
 </html>
