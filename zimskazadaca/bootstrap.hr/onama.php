446a119d36ec3e85fa994cabc22b0b59f1c9dc59
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-03 09:24:11 +0100

    update o nama

--- zimskazadaca/bootstrap.hr/onama.php
+++ zimskazadaca/bootstrap.hr/onama.php
@@ -11,25 +11,23 @@
                     <?php include_once 'navbar.php'; ?><br> 
                     
             </div>
-            
-            
-            
-            
-            
-              
-
-
-
-
+            <img src="slike/productplast.jpg" class="img-fluid" alt="...">
+            <a href="https://goo.gl/maps/Y13FSteqjubgsewX7" class="link-success" target="_blank" style="font-size:25px">Pogledaj gdje smo</a>
         </div>    
     </div>
-
-
-
+<br>
     <div class="container">
-        <div class="container text-center">
+        <div class="container text-center" style="background-color:darkgrey;">
 
-
+        <h3>Product plast jedan je od vodećih skandinavskih proizvođača vakumski oblikovanih termoplasta. Proizvodimo specijalizirane proizvode izrađene po narudžbi od termoplasta za industriju i druge svrhe. Udio izvoza je značajan i u porastu.
+            Tvrtka je osnovana 1972. godine u privatnom je vlasništvu. Položaj 20 km zapadno od Kristiansanda na jugu Norveške omogućuje dobru komunikaciju sa Skandinavijom i ostatkom kontinenta.</h3>
+        <hr>
+        <h4>Product Plast opremljen je modernom i učinkovitom proizvodnom opremom te kompetentnim zaposlenicima koji mogu riješiti većinu izazova koje tržište zahtijeva.
+        <hr>
+        <h4>Product Plast ima ambiciozan slogan; ”Pravo rješenje, u pravo vrijeme, s pravom kvalitetom”.
+        <hr>
+        <h4>Nastojimo to postići kroz posebnu posvećenost 4 glavna područja:<br>
+            Visoka kompetentnost, Fleksibilnost, Sigurnost opskrbe, Višak / Čvrstoća
         </div>    
     </div>
 
