19dbc4cb217c5ecd7b016538f73eab7ca9012cda
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-03 09:30:48 +0100

    update index

--- zimskazadaca/bootstrap.hr/index.php
+++ zimskazadaca/bootstrap.hr/index.php
@@ -16,17 +16,11 @@
                 Veličina redovne serije kreće se od 50 do 10 000 jedinica godišnje. Proces karakteriziraju niski troškovi alata, niski početni troškovi, kratka vremena i visok stupanj fleksibilnosti u pogledu odabira materijala.
                 <br><br>
               <img src="slike/produkt.jpeg" class="figure-img img-fluid rounded" alt="">
-              <figcaption class="figure-caption" style="font: size 25px;">
-              Od naših tipičnih proizvoda možemo spomenuti ploče za automobile: autobuse, kamione, građevinske strojeve i prenamjenu automobila. Industrija rashladnih sredstava: Rashladna oprema za komercijalne kuhinje, rashladne jedinice i druge srodne industrije. Pomoćni proizvodi: Razna pomagala za osobe s invaliditetom, kao što su invalidska kolica, skuteri, dizala. Displej: Razne fronte / paneli za strojeve i strojeve unutar maloprodaje.
+              <figcaption class="figure-caption" style="font-size: 25px;">
+              Od naših tipičnih proizvoda možemo spomenuti ploče za automobile: autobuse, kamione, građevinske strojeve i prenamjenu automobila. Industrija rashladnih sredstava: Rashladna oprema za komercijalne kuhinje, rashladne jedinice i druge srodne industrije.<br>
+              <hr>Pomoćni proizvodi: Razna pomagala za osobe s invaliditetom, kao što su invalidska kolica, skuteri, dizala. Displej: Razne fronte / paneli za strojeve i strojeve unutar maloprodaje.
               </figcaption>
             </figure>
-        </div>    
-    </div>
-
-    <div class="container">
-        <div class="container text-center" style="background-color:darkgrey;">
-            
-
         </div>    
     </div>
 
