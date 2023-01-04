e0942d33475b28f9d52db838380f61d41eff126c
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-04 16:41:46 +0100

    custom page
    mucenje sa responsive nav bar

---
+++ zimskazadaca/custom.hr/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/custom.css">
+</head>
+<body>
+    <div class="grid">
+        <div class="large-6 medium-6 small-12 cell">
+            <img src="slike/pp.jpg" alt="" style="width: 100%; height:auto; object-fit:cover;">
+        </div>
+
+        <nav class="navbar">
+            <div class="navname">Plast Product</div>
+                <a href="#" class="toggle-button">
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                    <span class="bar"></span>
+                </a>
+            <div class="navbar-links">
+                    <ul>
+                        <li><a href="#" class="active">Home page</a></li>
+                        <li><a href="proizvodi.php">Proizvodi</a></li>
+                        <li><a href="proces.php">Proces Proizvodnje</a></li>
+                        <li><a href="menadzment.php">Menadzment</a></li>
+                        <li><a href="onama.php">O nama</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+            </div>
+        </nav>
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+
+
+
+
+    <script src="custom.js"></script>
+</body>
+</html>
