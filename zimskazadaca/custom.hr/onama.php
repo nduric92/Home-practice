e77226dfe313b8aaa2056c9b55179f8626ae7271
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-05 13:54:30 +0100

    update

--- zimskazadaca/custom.hr/onama.php
+++ zimskazadaca/custom.hr/onama.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Plast Product</title>
+        <title>O nama</title>
         <link rel="stylesheet" href="css/custom.css">
     </head>
     <body>
@@ -22,11 +22,11 @@
                     </a>
                 <div class="navbar-links">
                         <ul>
-                            <li><a href="#" class="active">Home</a></li>
+                            <li><a href="index.php">Home</a></li>
                             <li><a href="product.php">Proizvodi</a></li>
                             <li><a href="proces.php">Proces Proizvodnje</a></li>
                             <li><a href="menadzment.php">Menadzment</a></li>
-                            <li><a href="onama.php">O nama</a></li>
+                            <li><a href="#" class="active">O nama</a></li>
                             <li><a href="login.php">Login</a></li>
                         </ul>
                 </div>
@@ -34,9 +34,6 @@
             <div class="large-6 medium-6 small-12 cell">
                 <img src="slike/pp.jpg" alt="" style="width: 100%; max-height: 300px; object-fit:cover;">
             </div>
-            <div class="text" style="color:white">bla bla</div>
-
-
         </div>
         <script src="custom.js"></script>
     </body>
