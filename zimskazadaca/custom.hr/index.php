13b35457a1e5bcedbbb93be2d16a93c43b4968ce
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-05 14:20:37 +0100

    update

--- zimskazadaca/custom.hr/index.php
+++ zimskazadaca/custom.hr/index.php
@@ -34,7 +34,17 @@
             <div class="large-6 medium-6 small-12 cell">
                 <img src="slike/pp.jpg" alt="" style="width: 100%; max-height: 300px; object-fit:cover;">
             </div>
-            <div class="text" style="color:white">bla bla</div>
+            <div class="text1">
+                <h1>Proizvodi vakumskog oblika koriste se u gotovo svim industrijama</h1>
+                <p>Veličina redovne serije kreće se od 50 do 10 000 jedinica godišnje. Proces karakteriziraju niski troškovi alata, niski početni troškovi, kratka vremena i visok stupanj fleksibilnosti u pogledu odabira materijala.</p>
+            </div>
+            <div class="slikaindex">
+            <img src="slike/volvo1.jpg" alt="" style="width: 100%; height: 100%;">
+            </div>
+            <div class="text1">
+            Od naših tipičnih proizvoda možemo spomenuti ploče za automobile: autobuse, kamione, građevinske strojeve i prenamjenu automobila. Industrija rashladnih sredstava: Rashladna oprema za komercijalne kuhinje, rashladne jedinice i druge srodne industrije.<hr style="width:80%">
+            Pomoćni proizvodi: Razna pomagala za osobe s invaliditetom, kao što su invalidska kolica, skuteri, dizala. Displej: Razne fronte / paneli za strojeve i strojeve unutar maloprodaje.
+            </div>
 
 
         </div>
