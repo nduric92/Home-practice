23fb6cafc55b18af04e90cdf435ea187995fdc05
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-09 09:52:09 +0100

    update na custom

--- zimskazadaca/custom.hr/onama.php
+++ zimskazadaca/custom.hr/onama.php
@@ -33,6 +33,26 @@
             <div class="large-6 medium-6 small-12 cell">
                 <img src="slike/pp.jpg" alt="" style="width: 100%; max-height: 300px; object-fit:cover;">
             </div>
+
+            <h2 style="text-align:center; color:White;">Pronadji nas</h2>
+
+             <!-- Picture -->
+             <div class="large-6 medium-6 small-12 cell">
+                <img src="slike/productplast.jpg" alt="" style="width: 100%; max-height: 300px; object-fit:cover;">
+            </div>
+            <div style="font-size: 25px; text-align:center">
+            <a href="https://goo.gl/maps/Y13FSteqjubgsewX7" class="link-success" target="_blank" style="font-size:25px">Pogledaj gdje smo</a><hr style="width:80%">
+            </div>
+            <div class="text1">
+                Product plast jedan je od vodećih skandinavskih proizvođača vakumski oblikovanih termoplasta. Proizvodimo specijalizirane proizvode izrađene po narudžbi od termoplasta za industriju i druge svrhe. Udio izvoza je značajan i u porastu. Tvrtka je osnovana 1972. godine u privatnom je vlasništvu. Položaj 20 km zapadno od Kristiansanda na jugu Norveške omogućuje dobru komunikaciju sa Skandinavijom i ostatkom kontinenta.<hr style="width:80%">
+                Product Plast opremljen je modernom i učinkovitom proizvodnom opremom te kompetentnim zaposlenicima koji mogu riješiti većinu izazova koje tržište zahtijeva.<hr style="width:80%">
+                Product Plast ima ambiciozan slogan; ”Pravo rješenje, u pravo vrijeme, s pravom kvalitetom”.<hr style="width:80%">
+                Nastojimo to postići kroz posebnu posvećenost 4 glavna područja:<br>
+                Visoka kompetentnost, Fleksibilnost, Sigurnost opskrbe, Višak / Čvrstoća
+                <br>***
+            </div>            
+
+            <?php include_once 'podnozje.php'; ?>
         </div>
         <script src="custom.js"></script>
     </body>
--- zimskazadaca/custom.hr/podnozje.php
+++ zimskazadaca/custom.hr/podnozje.php
@@ -1,1 +1,1 @@
-<hr style="width:90%;"><h6 style="color: white;"> &copy; <?php echo date('Y'); ?> Plast Product
+<hr style="width:80%;"><h6 style="color: white;"> &copy; <?php echo date('Y'); ?> Plast Product
