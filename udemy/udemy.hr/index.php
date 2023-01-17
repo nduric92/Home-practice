a0f75c835d10551bb2e356ed39b6e47be3fb1033
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-17 16:09:43 +0100

    Update index.php

--- udemy/udemy.hr/index.php
+++ udemy/udemy.hr/index.php
@@ -20,12 +20,43 @@ echo '<hr>';
 $car1["mercedes"] = 60.300;
 echo $car1["mercedes"];
 
+
+
+
 echo '<hr>';
+
+
+
 
 //FOREACH primer
 foreach ($cars as $key => $value) {
     echo 'My ' . $key . ' has ' . $value . ' mileage <br>';
 }
+
+
+
+
+echo '<hr>';
+$automobili=['audi' => 174000, 'bmw' => 200131, 'polo' => 175500];{
+    print_r($automobili);
+}
+
+echo '<hr>';
+
+foreach($automobili as $kljuc => $vrednost) {
+    echo 'Moj automobil ' . $kljuc . ' ima ' . $vrednost . ' predjenih kilometara <br>';
+}
+
+echo '<hr>';
+
+$kolegij=['Matematika' => 5, 'Fizika' => 3, 'Racunalstvo' => 4, 'Informatika' => 5, 'Robotika' => 5];
+
+foreach($kolegij as $predmet => $ocijena){
+    echo 'Predmet ' . $predmet . ' je ocjenjen ' . $ocijena . ' ocjenom <br>';
+}
+
+
+
 
 
 
