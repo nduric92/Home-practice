9e5a929d5371a3baef740e0b85a0c4ed7b6a7078
Nemanja Duric <117755538+nduric92@users.noreply.github.com>
2023-01-26 12:50:35 +0100

    Update buildinfunctions.php

--- udemy/udemy.hr/buildinfunctions.php
+++ udemy/udemy.hr/buildinfunctions.php
@@ -21,6 +21,68 @@
 <hr>
 
 <?php 
+//==========================//
+//  matematicke funkcije    //
+//==========================//
+
+//  CEIL
+echo ceil(1.50) . '<br>';       //ispisuje br 2, podize na prvi sledeci celi br
+
+//FLOOR
+echo floor(1.50) . '<br>';      //ispisuje br 1, spusta na prvi nizi celi br
+
+//ROUND
+echo round(0.54335, 2) . '<br>'; //ispisuje samo dva broja decimala jer smo tako uneli u zagradu kao drugu vrednost
+
+echo exp(4);
+
+echo '<hr>';
+
+//==============================//
+//  string build-in functions   //
+//==============================//
+
+
+// STRING LENGHT
+$str = 'Today is a sunny day';
+
+strlen($str); //counts nummber of caracters in variable
+
+
+
+//STRING WORD COUNT
+$firstName = 'My first name is Nemanja';
+
+echo str_word_count($firstName) . '<br>';
+
+
+
+// how to find text within a string
+
+$email = 'info@nemanjaduric.com';
+if(strpos($email,'@')){
+    echo 'This is a valid email';
+}else{
+    echo 'This is a email input field';
+}
+echo '<hr>';
+
+
+//UCwords - every first letter of a word is uppercase
+
+echo ucwords('what is your name' . '<br>');
+
+//whole string to uppercase
+
+echo strtoupper('what is your name' . '<br>');
+
+//whole string to lowercase
+
+echo strtolower('what is your name' . '<br>');
+
+//=============================//
+//  Array build-in functions   //
+//=============================//
 
 
 
